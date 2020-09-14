284adc1526d6518d4d5e20010eba8c6d8ac4bf04
SpiritCroc <dev@spiritcroc.de>
2020-09-14 10:08:07 +0200

    setup.sh: linking these not required?

--- setup.sh
+++ setup.sh
@@ -23,9 +23,9 @@ $yarn install
 popd
 
 pushd element-desktop
-$yarn link matrix-js-sdk
-$yarn link matrix-react-sdk
-$yarn link riot-web
+#$yarn link matrix-js-sdk
+#$yarn link matrix-react-sdk
+#$yarn link riot-web
 $yarn install
 ln -s ../element-web/webapp ./ || true
 popd
