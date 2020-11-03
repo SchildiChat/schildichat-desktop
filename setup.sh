de5fa4e9e4bc31e98707b8f961bf70de862eb3df
su-ex <codeworks@supercable.onl>
2020-11-03 02:08:06 +0100

    setup.sh: get rid of maybe preexisting yarn links

--- setup.sh
+++ setup.sh
@@ -5,12 +5,14 @@ set -e
 yarn=yarnpkg
 
 pushd matrix-js-sdk
+$yarn unlink &>/dev/null || true
 $yarn link
 $yarn install
 popd
 
 pushd matrix-react-sdk
 $yarn link matrix-js-sdk
+$yarn unlink &>/dev/null || true
 $yarn link
 $yarn install
 popd
@@ -18,14 +20,10 @@ popd
 pushd element-web
 $yarn link matrix-js-sdk
 $yarn link matrix-react-sdk
-$yarn link
 $yarn install
 popd
 
 pushd element-desktop
-#$yarn link matrix-js-sdk
-#$yarn link matrix-react-sdk
-#$yarn link riot-web
 $yarn install
 ln -s ../element-web/webapp ./ || true
 popd
