e153a52c84fd24e6fa3f1c3378c60caa4e1fb51e
Yureka <yuka@yuka.dev>
2021-10-10 23:34:08 +0200

    nix: update nixpkgs, unvendor yarn2nix

--- nix/overlay.nix
+++ nix/overlay.nix
@@ -1,5 +1,4 @@
 final: prev: {
-  yarn2nix-sc = final.callPackage ./yarn2nix { };
   cleanSchildichatDesktopSource = src: with final.lib; cleanSourceWith {
     filter = name: type: cleanSourceFilter name type
       && !(hasInfix "/node_modules/" name)
@@ -8,14 +7,11 @@ final: prev: {
     inherit src;
   };
   schildichat-web = final.callPackage ./schildichat-web.nix {
-    inherit (final.yarn2nix-sc) mkYarnModules;
   };
   schildichat-desktop = final.callPackage ./schildichat-desktop.nix {
-    inherit (final.yarn2nix-sc) mkYarnPackage;
     inherit (final.darwin.apple_sdk.frameworks) Security AppKit CoreServices;
   };
   schildichat-desktop-wayland = final.callPackage ./schildichat-desktop.nix {
-    inherit (final.yarn2nix-sc) mkYarnPackage;
     inherit (final.darwin.apple_sdk.frameworks) Security AppKit CoreServices;
     useWayland = true;
   };
