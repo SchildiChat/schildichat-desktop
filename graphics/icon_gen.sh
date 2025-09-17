4d4dcf1f7baf4cfaa1784940e479fd432d7e9af1
SpiritCroc <dev@spiritcroc.de>
2025-09-17 08:35:55 +0200

    Element v1.11.112

--- graphics/icon_gen.sh
+++ graphics/icon_gen.sh
@@ -25,6 +25,9 @@ export_square() {
     in="$2"
     out="$3"
     export_rect "$1" "$size" "$in" "$out"
+    if [[ "$out" == *.png ]]; then
+        pngcrush -ow "$out"
+    fi
 }
 
 repo_dir="$SCHILDI_ROOT/element-web"
@@ -50,50 +53,22 @@ export_rect 256 125 "$mydir/feature_image.svg" "$base_out/mstile-310x150.png"
 magick "$base_out/48.png" "$base_out/favicon.ico"
 rm "$base_out/48.png" # this was only created for favicon.ico
 
-for f in "$base_out"/*.png; do
-    pngcrush -ow "$f"
-done
-
 
 cp "$mydir/ic_launcher_sc.svg" "$repo_dir/res/themes/element/img/logos/element-logo.svg"
 
 export_square 320 "$mydir/ic_launcher_sc.svg" "$repo_dir/res/themes/element/img/logos/element-app-logo.png"
 
+
 repo_dir="$SCHILDI_ROOT/element-desktop"
-base_out="$repo_dir/res/img"
-
-export_square 256 "$mydir/ic_launcher_sc.svg" "$base_out/element.png"
-magick "$base_out/element.png" "$base_out/element.ico"
-
-# TODO monochrome icon? Unless https://github.com/element-hq/element-desktop/pull/1934 is what we'll end with
-export_square 256 "$mydir/ic_launcher_sc.svg" "$base_out/monochrome.png"
-magick "$base_out/element.png" "$base_out/monochrome.ico"
-
-for f in "$base_out"/*.png; do
-    pngcrush -ow "$f"
-done
-
-
-
-
 base_out="$repo_dir/build"
 
-for i in 16 24 48 64 96 128 256 512 1024; do
-    export_square "$i" "$mydir/ic_launcher_sc.svg" "$base_out/icons/$i"x"$i.png"
-done
-
-export_square "320" "$mydir/ic_launcher_sc.svg" "$base_out/install-spinner.png"
-pngcrush "$base_out/install-spinner.png"
-magick "$base_out/install-spinner.png" "$base_out/install-spinner.gif"
-rm  "$base_out/install-spinner.png"
-
-magick "$base_out/icons/256x256.png" "$base_out/icon.ico"
-magick "$base_out/icons/1024x1024.png" "$base_out/icon.icns"
-rm "$base_out/icons/1024x1024.png"
-
-for f in "$base_out/icons"/*.png; do
-    pngcrush -ow "$f"
-done
+export_square 512 "$mydir/ic_launcher_sc.svg" "$base_out/icon.png"
+export_square 256 "$mydir/ic_launcher_sc.svg" "$base_out/icon256_tmp.png"
+magick "$base_out/icon256_tmp.png" "$base_out/icon.ico"
+rm "$base_out/icon256_tmp.png"
+export_square 1024 "$mydir/ic_launcher_sc.svg" "$base_out/icon1024_tmp.png"
+magick "$base_out/icon1024_tmp.png" "$base_out/icon.icns"
+rm "$base_out/icon1024_tmp.png"
 
 
 if [[ "$automatic_commit" == [Yy]* ]]; then
